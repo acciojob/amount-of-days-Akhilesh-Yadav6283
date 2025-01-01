f0aa8640d55a44d21a8641ca4a34169a80ee4361
Akhilesh-Yadav6283 <135110548+Akhilesh-Yadav6283@users.noreply.github.com>
2025-01-01 12:00:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function daysOfAYear( ) {
+
+	let year = promt();
+
+	if(year%4==0 || year%100==0){
+		if(year%400==0 && year%100==0){
+			console.log(366);
+		}
+		else{
+			console.log(365);
+		}
+	}else{
+		console.log(365);
+	}
+	
+	
+}
